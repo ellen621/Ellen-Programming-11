2cb6550d9c5de3927775f21ffda8ff12c468f0a1
ellen621 <61611190+ellen621@users.noreply.github.com>
2020-04-03 15:51:01 -0700

    RockPaperScissors

--- RockPaperScissors/src/Main.java
+++ RockPaperScissors/src/Main.java
@@ -1,27 +1,37 @@
+//Import Scanner so I can get user input
 import java.util.Scanner;
-//for a tie put Draw! for a win put You Win! for loss put You Lose! and then put computer choice: choice player choice.
-// for invalid put invalid selection please play again.
+
 public class Main {
     public static void main(String[] args) {
+        //Set up Scanner
         Scanner scan = new Scanner(System.in);
+        //Prompt to get String
         System.out.println("Choose rock paper or scissors. 'r' for rock, 'p' for paper and 's' for scissors");
+        //Collecting the input
         String input = scan.nextLine();
+        //Generate random number then multiply by 100 so it's between 1-100
         int choice = (int)Math.random()*100;
-        if (input == "r"){
+        //Use if/elseif/elseif/else so that I have something for every occurrence
+        //Only explained the first one because I repeated the code
+        if (input.equals("r")){
+            //If the int choice was in the first 1/3 it was scissors
             if (choice <= 33) {
                 System.out.println("You Win!");
                 System.out.println("Computer Choice: Scissors     Player Choice: Rock");
             }
+            //If it was between 34 and 68, it was Paper
             else if (choice >= 67) {
                 System.out.println("You Lose!");
                 System.out.println("Computer Choice: Paper     Player Choice: Rock");
             }
+            //and since all that remained was Rock, and numbers above 67, I used an else statement
             else{
                 System.out.println("Draw!");
                 System.out.println("Computer Choice: Rock     Player Choice: Rock");
             }
         }
-        else if (input == "p"){
+        //repeat but this time user input was paper
+        else if (input.equals("p")){
             if (choice <= 33) {
                 System.out.println("You Lose!");
                 System.out.println("Computer Choice: Scissors     Player Choice: Paper");
@@ -35,7 +45,8 @@ public class Main {
                 System.out.println("Computer Choice: Rock     Player Choice: Paper");
             }
         }
-        else if (input== "s"){
+        //repeat but it was scissors This Time
+        else if (input.equals("s")){
             if (choice <= 33) {
                 System.out.println("Draw!");
                 System.out.println("Computer Choice: Scissors     Player Choice: Scissors");
@@ -49,9 +60,11 @@ public class Main {
                 System.out.println("Computer Choice: Rock     Player Choice: Scissors");
             }
         }
+        //In the occurrence that they put in an invalid letter/string/integer, I had an else statement
         else{
             System.out.println("Invalid selection please play again.");
         }
 
     }
 }
+
